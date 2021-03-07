9c432903c48e040433621581f5403abc14e96893
garcanm8852 <a20garcanm8852@ies-mardecadiz.com>
2021-03-07 13:52:45 +0100

    New Header

--- src/components/header.js
+++ src/components/header.js
@@ -1,34 +1,89 @@
 import React from "react";
-
-import {Navbar, Nav, NavDropdown, Button} from "react-bootstrap";
-import * as headerStyles  from "../styles/header.module.css";
 import logo from "../IMG/atendidogs-4.png";
 
 export default (props) => (
-    <Navbar bg="light" expand="lg" className="justify-content-center">
-        <Navbar.Brand href="." className={headerStyles.header} >
-        <img src={logo} alt="Logo Clinica Veterinaria Atendidogs" className={headerStyles.logo}/>
-        <span className={headerStyles.blue1}>Atendidogs</span>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto">
-                <Nav.Link href=".">Inicio</Nav.Link>
-                <Nav.Link href="#link">Equipo</Nav.Link>
-                <NavDropdown title="Servicios" id="basic-nav-dropdown">
-                    <NavDropdown.Item href=".">Servicios Principales</NavDropdown.Item>
-                    <NavDropdown.Item href=".">Veterinaria</NavDropdown.Item>
-                    <NavDropdown.Item href=".">Peluquería</NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item href=".">Adopción</NavDropdown.Item>
-                </NavDropdown>
-                <Nav.Link href=".">Instalaciones</Nav.Link>
-                <Nav.Link href=".">Tienda</Nav.Link>
-                <NavDropdown title="Contáctanos" id="basic-nav-dropdown">
-                    <NavDropdown.Item href=".">Datos de contacto</NavDropdown.Item>
-                    <NavDropdown.Item href=".">Cita Previa</NavDropdown.Item>
-                </NavDropdown>
-            </Nav>
-        </Navbar.Collapse>
-    </Navbar>
+    <nav className="navbar navbar-expand-lg navbar-light
+    justify-content-center">
+        <nav className="navbar navbar-light">
+            <a className="navbar-brand" href="./index.html">
+                <img src={logo} width="30" height="30" alt="Clínica Veterinaria Atendidogs" loading="lazy"/> Clínica Veterinaría
+                <span>Atendidogs</span>
+            </a>
+
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav text-right">
+                    <li className="nav-item">
+                        <a className="nav-link active" alt="Inicio" href="./index.html">Inicio<span className="sr-only">(current)</span></a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" alt="Equipo" href="./equipo.html">Equipo</a>
+                    </li>
+                    <li className="nav-item dropdown">
+                        <a className="nav-link dropdown-toggle" alt="Servicios" href="./servicios.html" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Servicios
+                        </a>
+                        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a className="dropdown-item" href="./servicios.html" alt="Servicios Principales">Servicios Principales</a>
+                            <a className="dropdown-item" href="./veterinaria.html" alt="Veterinaria">Veterinaria</a>
+                            <a className="dropdown-item" href="./peluqueria.html" alt="Peluqueria">Peluquería</a>
+                            <div className="dropdown-divider"></div>
+                            <a className="dropdown-item" href="./adopcion.html" alt="Adopcion">Adopción</a>
+                        </div>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="./instalaciones.html" alt="Instalaciones">Instalaciones</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="./tienda.html" alt="Tienda">Tienda</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="./noticias.html" alt="Noticias">Noticias</a>
+                    </li>
+                    <li className="nav-item dropdown">
+                        <a className="nav-link dropdown-toggle" href="./contacto.html" alt="Contáctanos" id="navbarDropdownContacto" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Contáctanos
+                        </a>
+                        <div className="dropdown-menu" aria-labelledby="navbarDropdownContacto">
+                            <a className="dropdown-item" href="./contacto.html" alt="Contacto">Datos de contacto</a>
+                            <a className="dropdown-item" href="./cita-previa.html" alt="Cita Previa">Cita Previa</a>
+                        </div>
+                    </li>
+                </ul>
+                <ul className="nav navbar-nav flex-row justify-content-between ml-auto">
+                    <li className="dropdown order-1">
+                        <button type="button" id="dropdownMenu1" data-toggle="dropdown" className="btn btn-outline-secondary dropdown-toggle" alt="Iniciar Sesión">Iniciar Sesión <span
+                                className="caret"></span></button>
+                        <ul className="dropdown-menu dropdown-menu-right mt-2">
+                            <li className="px-3 py-2">
+                                <form className="form" role="form">
+                                    <div className="form-group">
+                                        <input id="emailInput" placeholder="Email" className="form-control form-control-sm" type="text" required="" alt="Email"/>
+                                    </div>
+                                    <div className="form-group">
+                                        <input id="passwordInput" placeholder="Contraseña" className="form-control form-control-sm" type="text" required="" alt="Contraseña"/>
+                                    </div>
+                                    <div className="form-group">
+                                        <button type="submit" className="btn btn-primary btn-block" alt="Iniciar Sesión">Iniciar Sesión</button>
+                                    </div>
+                                    <div className="form-group text-center">
+                                        <small><a href="./login.html" data-toggle=""
+                                                data-target="#modalPassword" alt="¿Olvidó su contraseña?">¿Olvidó su contraseña?</a></small>
+                                    </div>
+                                    <div className="form-group text-center">
+                                        <small><a href="./login.html" data-toggle="" data-target="#modalPassword" alt="¿No
+                                            tienes cuenta? Registrarse">¿No
+                                                tienes cuenta? Registrarse</a></small>
+                                    </div>
+                                </form>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </nav>
 );
---
+++ src/components/header_ReactBootsrap.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+import {Navbar, Nav, NavDropdown, Button} from "react-bootstrap";
+import * as headerStyles  from "../styles/header.module.css";
+import logo from "../IMG/atendidogs-4.png";
+
+export default (props) => (
+    <Navbar bg="light" expand="lg" className="justify-content-center">
+        <Navbar.Brand href="." className={headerStyles.header} >
+        <img src={logo} alt="Logo Clinica Veterinaria Atendidogs" className={headerStyles.logo}/>
+        <span className={headerStyles.blue1}>Atendidogs</span>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+                <Nav.Link href=".">Inicio</Nav.Link>
+                <Nav.Link href="#link">Equipo</Nav.Link>
+                <NavDropdown title="Servicios" id="basic-nav-dropdown">
+                    <NavDropdown.Item href=".">Servicios Principales</NavDropdown.Item>
+                    <NavDropdown.Item href=".">Veterinaria</NavDropdown.Item>
+                    <NavDropdown.Item href=".">Peluquería</NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href=".">Adopción</NavDropdown.Item>
+                </NavDropdown>
+                <Nav.Link href=".">Instalaciones</Nav.Link>
+                <Nav.Link href=".">Tienda</Nav.Link>
+                <NavDropdown title="Contáctanos" id="basic-nav-dropdown">
+                    <NavDropdown.Item href=".">Datos de contacto</NavDropdown.Item>
+                    <NavDropdown.Item href=".">Cita Previa</NavDropdown.Item>
+                </NavDropdown>
+            </Nav>
+        </Navbar.Collapse>
+    </Navbar>
+);
